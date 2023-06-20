9d6e58ada70fb639244c6536eb3df74aed0c9c8a
Nathan <nkspaun@gmail.com>
2023-06-19 20:18:53 -0700

    First commit

---
+++ ChessBoxing/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ChessBoxing
+//
+//  Created by NathanSpaun on 6/19/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
