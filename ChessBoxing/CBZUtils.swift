34ec6a286eddb84982f6bde93ead6215d9fb1cdd
Nathan <nkspaun@gmail.com>
2023-07-12 18:25:37 -0700

    Add matrix manipulations

--- ChessBoxing/CBZUtils.swift
+++ ChessBoxing/CBZUtils.swift
@@ -19,3 +19,67 @@ func rotateVector(vector: simd_float4, angle: Float, axis: simd_float3) -> simd_
     
     return rotatedQuaternion.vector
 }
+
+func getRotationMatrix(axis: simd_float3, angle: Float) -> simd_float4x4 {
+    let normalizedAxis = normalize(axis)
+
+    // Convert the angle to radians
+    let radians = angle
+
+    // Calculate the quaternion components
+    let halfAngle = radians * 0.5
+    let sinHalfAngle = sin(halfAngle)
+    let cosHalfAngle = cos(halfAngle)
+
+    // Calculate the quaternion
+    let quaternion = simd_float4(normalizedAxis.x * sinHalfAngle,
+                                 normalizedAxis.y * sinHalfAngle,
+                                 normalizedAxis.z * sinHalfAngle,
+                                 cosHalfAngle)
+
+    // Calculate the rotation matrix
+    let x2 = quaternion.x * quaternion.x
+    let y2 = quaternion.y * quaternion.y
+    let z2 = quaternion.z * quaternion.z
+    let xy = quaternion.x * quaternion.y
+    let xz = quaternion.x * quaternion.z
+    let yz = quaternion.y * quaternion.z
+    let wx = quaternion.w * quaternion.x
+    let wy = quaternion.w * quaternion.y
+    let wz = quaternion.w * quaternion.z
+
+    let rotationMatrix = simd_float4x4(columns: (
+        simd_float4(1 - 2 * (y2 + z2), 2 * (xy - wz), 2 * (xz + wy), 0.0),
+        simd_float4(2 * (xy + wz), 1 - 2 * (x2 + z2), 2 * (yz - wx), 0.0),
+        simd_float4(2 * (xz - wy), 2 * (yz + wx), 1 - 2 * (x2 + y2), 0.0),
+        simd_float4(0.0, 0.0, 0.0, 1.0)
+        ))
+    return rotationMatrix
+}
+
+func getScaleMatrix(scaleFactor: Float) -> simd_float4x4 {
+    return simd_float4x4(columns: (
+        simd_float4(scaleFactor, 0, 0, 0),
+        simd_float4(0, scaleFactor, 0, 0),
+        simd_float4(0, 0, scaleFactor, 0),
+        simd_float4(0, 0, 0, 1)
+    ))
+}
+
+func getTranslationMatrix(translation: simd_float4) -> simd_float4x4 {
+    assert(translation[3] == 0)
+    return simd_float4x4(columns: (
+        simd_float4(1, 0, 0, translation[0]),
+        simd_float4(0, 1, 0, translation[1]),
+        simd_float4(0, 0, 1, translation[2]),
+        simd_float4(0, 0, 0, 1)
+    ))
+}
+
+func getProjectionMatrix(projectionPlaneDepth: Float, vh: Float, ch: Float, vw: Float, cw:Float) -> simd_float3x4 {
+    return simd_float3x4(columns: (
+        simd_float4(projectionPlaneDepth*cw/vw, 0, 0, 0),
+        simd_float4(0, projectionPlaneDepth*ch/vh, 0, 0),
+        simd_float4(0, 0, 1, 0)
+    ))
+}
--- ChessBoxingTests/ChessBoxingTests.swift
+++ ChessBoxingTests/ChessBoxingTests.swift
@@ -14,7 +14,7 @@ final class ChessBoxingTests: XCTestCase {
 
     override func setUpWithError() throws {
         let device: MTLDevice = MTLCreateSystemDefaultDevice()!
-        self.sceneModel = CBZSceneModel(device: device)
+        self.sceneModel = CBZSceneModel(device: device, viewPort: CBZViewport(width: 1, height: 1, distanceFromCamera: 1))
     }
 
     override func tearDownWithError() throws {
@@ -43,6 +43,100 @@ final class ChessBoxingTests: XCTestCase {
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func testScaleMatrix() {
+        let scaleMatrix = getScaleMatrix(scaleFactor: 2)
+        XCTAssert(scaleMatrix[0][0] == 2)
+    }
+    
+    func testRotationMatrix() {
+        let identity = simd_float4x4(columns: (
+            simd_float4(1, 0, 0, 0),
+            simd_float4(0, 1, 0, 0),
+            simd_float4(0, 0, 1, 0),
+            simd_float4(0, 0, 0, 1)
+        ))
+        let ir = getRotationMatrix(axis: simd_float3(1,0,0), angle: 0);
+        XCTAssert(areMatricesEqual(ir, identity))
+        
+        let t1 = simd_float4x4(columns: (
+            simd_float4(1, 0, 0, 0),
+            simd_float4(0, 0.5403, -0.84147, 0),
+            simd_float4(0, 0.84147, 0.5403, 0),
+            simd_float4(0, 0, 0, 1)
+        ))
+        let r1 = getRotationMatrix(axis: simd_float3(1,0,0), angle: 1);
+        XCTAssert(areMatricesEqual(t1, r1))
+        
+        let t2 = simd_float4x4(columns: (
+            simd_float4(-0.4161468, -0.6429704,  0.6429704, 0),
+            simd_float4(0.6429704,  0.2919266,  0.7080734, 0),
+            simd_float4(-0.6429704,  0.7080734,  0.2919266, 0),
+            simd_float4(0, 0, 0, 1)
+        ))
+        let r2 = getRotationMatrix(axis: simd_float3(0,1,1), angle: 2);
+        XCTAssert(areMatricesEqual(t2, r2))
+    }
+    
+    func testTranslationMatrix() {
+        let identity = simd_float4x4(columns: (
+            simd_float4(1, 0, 0, 0),
+            simd_float4(0, 1, 0, 0),
+            simd_float4(0, 0, 1, 0),
+            simd_float4(0, 0, 0, 1)
+        ))
+        let ir = getTranslationMatrix(translation: simd_float4(0,0,0,0))
+        XCTAssert(areMatricesEqual(ir, identity))
+        
+        let t1 = simd_float4x4(columns: (
+            simd_float4(1, 0, 0, 1),
+            simd_float4(0, 1, 0, 2),
+            simd_float4(0, 0, 1, 3),
+            simd_float4(0, 0, 0, 1)
+        ))
+        let r1 = getTranslationMatrix(translation: simd_float4(1,2,3,0))
+        XCTAssert(areMatricesEqual(t1, r1))
+    }
+    
+    func testProjectionMatrix() {
+        let identity = simd_float3x4(columns: (
+            simd_float4(1, 0, 0, 0),
+            simd_float4(0, 1, 0, 0),
+            simd_float4(0, 0, 1, 0)
+        ))
+        let ir = getProjectionMatrix(projectionPlaneDepth: 1, vh: 1, ch: 1, vw: 1, cw: 1)
+        XCTAssert(are3x4MatricesEqual(ir, identity))
+        
+        let t1 = simd_float3x4(columns: (
+            simd_float4(0.5, 0, 0, 0),
+            simd_float4(0, 0.5, 0, 0),
+            simd_float4(0, 0, 1, 0)
+        ))
+        let r1 = getProjectionMatrix(projectionPlaneDepth: 0.5, vh: 1, ch: 1, vw: 1, cw: 1)
+        XCTAssert(are3x4MatricesEqual(t1, r1))
+    }
+    
+    func areMatricesEqual(_ matrix1: simd_float4x4, _ matrix2: simd_float4x4) -> Bool {
+        for i in 0..<4 {
+            for j in 0..<4 {
+                if abs(matrix1[i][j] - matrix2[i][j]) > 0.001 {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    
+    func are3x4MatricesEqual(_ matrix1: simd_float3x4, _ matrix2: simd_float3x4) -> Bool {
+        for i in 0..<3 {
+            for j in 0..<4 {
+                if abs(matrix1[i][j] - matrix2[i][j]) > 0.001 {
+                    return false
+                }
+            }
+        }
+        return true
     }
 
 }
